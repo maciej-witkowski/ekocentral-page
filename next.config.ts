4b711be1a4b0d3a116a05a249671a1f3c4b374c3
Maciej Witkowski <deskorian@MacBook-Pro-Maciej.local>
2026-05-01 15:20:07 +0200

    Fix issue with images rendering

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
   output: 'export',
+  basePath: '/ekocentral-page',
+  assetPrefix: '/ekocentral-page/',
   images: {
     unoptimized: true,
   },
